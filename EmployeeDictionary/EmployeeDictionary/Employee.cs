825d7e50cf729d4a853c2ebe589e0c7606636fe1
weilaa36 <37908253+weilaa36@users.noreply.github.com>
2018-03-29 11:07:03 -0500

    Add files via upload
    This is the Dictionary Lab from day 12

---
+++ EmployeeDictionary/EmployeeDictionary/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; } // like the PK column
+
+        public override string ToString()
+        {
+            return "ID: " + Id.ToString() + " First Name: " +FirstName + " Last Name: " + LastName;
+        }
+
+    }
+}
---
+++ EmployeeDictionary/EmployeeDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new Dictionary<int, Employee>();
+            employees.Add(101, 
+                new Employee { Id = 101, FirstName= "Jesse", LastName = "Smith"});
+
+            employees.Add(102,
+                new Employee{ Id = 102, FirstName = "George", LastName = "Washington"});
+            employees.Add(103,
+                new Employee{ Id = 103, FirstName = "Will", LastName = "Turner"});
+            Console.WriteLine(employees[101]);
+            Console.WriteLine(employees[102]);
+            Console.WriteLine(employees[103]);
+            Console.ReadLine();
+        }
+    }
+}
